788396c3be710362a649458be4ac53528e445c90
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2024-05-14 19:38:38 +0500

    service dto

--- backend/src/main/java/com/astami/backend/dto/ServiceDto.java
+++ backend/src/main/java/com/astami/backend/dto/ServiceDto.java
@@ -5,10 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Time;
+import java.time.Duration;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class ServiceDto {
     private long id;
+    private String title;
+    private String description;
+    private float price;
+    private Duration duration;
+    private Time availableFrom;
+    private Time availableTo;
+    private List<Long> employees;
+    private long branchId;
 }
--- backend/src/main/java/com/astami/backend/mapper/ServiceMapper.java
+++ backend/src/main/java/com/astami/backend/mapper/ServiceMapper.java
@@ -1,12 +1,21 @@
 package com.astami.backend.mapper;
 
 import com.astami.backend.dto.ServiceDto;
+import com.astami.backend.model.Employee;
 import com.astami.backend.model.Service;
 
 public class ServiceMapper {
 
     public static ServiceDto mapToDto(Service service) {
         return ServiceDto.builder()
+                .id(service.getId())
+                .title(service.getTitle())
+                .description(service.getDescription())
+                .price(service.getPrice())
+                .availableFrom(service.getAvailableFrom())
+                .availableTo(service.getAvailableTo())
+                .duration(service.getDuration())
+                .employees(service.getEmployees().stream().map(Employee::getId).toList())
                 .build();
     }
 }
